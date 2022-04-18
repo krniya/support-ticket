dc9cc77e602622be7164129e0b3137dc20208838
Nitish Kumar <krnitish@live.in>
2022-04-18 23:01:10 +0530

    Configured Error & Exception handler using middleware
    - Added errorMiddleware
    - Installed express-async-handler to use async-await

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { errorHandler } = require("./middleware/errorMiddleware");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server running at PORT ${PORT}`);
