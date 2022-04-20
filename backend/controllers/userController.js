2c847d89fd46bd45298e275784d55d75e1eb4453
Nitish Kumar <krnitish@live.in>
2022-04-20 23:10:31 +0530

    Configured User Modal
    - Created model folder for DB models
    - Added functionality to register new user in userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,6 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
 
 // * @desc        Register new user
 // * @route       /api/users
@@ -10,6 +12,35 @@ const registerUser = asyncHandler(async (req, res) => {
     if (!name || !email || !password) {
         res.status(400);
         throw new Error("Please include all fields.");
+    }
+
+    // Find if user already exist
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+        res.status(400);
+        throw new Error("User already exist");
+    }
+
+    // Hash Password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create User
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid user data");
     }
 });
 
