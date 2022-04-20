09dbe5da380622bf9d08c881db197aeb4f892f01
Nitish Kumar <krnitish@live.in>
2022-04-20 23:27:00 +0530

    Added Login functionality & JsonWebToken Added
    - Installed JsonWebToken
    - Login functionality Added
    - Return of JsonWebToken when register or login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
 // * @desc        Register new user
 // * @route       /api/users
@@ -30,6 +31,7 @@ const registerUser = asyncHandler(async (req, res) => {
         name,
         email,
         password: hashedPassword,
+        token: generateToken(user._id),
     });
 
     if (user) {
@@ -48,8 +50,29 @@ const registerUser = asyncHandler(async (req, res) => {
 // * @route       /api/users/login
 // * @access      Public
 const loginUser = asyncHandler(async (req, res) => {
-    res.send("Register Route");
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    // Check user and password match
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(401);
+        throw new Error("Invalid Credentials");
+    }
 });
+
+// Generate Token
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: "30d",
+    });
+};
 
 module.exports = {
     registerUser,
