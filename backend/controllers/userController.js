2aa248194856a829a9b9317b5e026710da69e197
Nitish Kumar <krnitish@live.in>
2022-04-22 23:49:54 +0530

    Authentication and routes protection added
    - Added authMiddleware to autheticate routes
    - GET(api/users/me) route added to recive logged in user info

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -67,6 +67,18 @@ const loginUser = asyncHandler(async (req, res) => {
     }
 });
 
+// * @desc        Get current user
+// * @route       /api/users/me
+// * @access      Private
+const getMe = asyncHandler(async (req, res) => {
+    const user = {
+        id: req.user._id,
+        email: req.user.email,
+        name: req.user.name,
+    };
+    res.status(200).json(user);
+});
+
 // Generate Token
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -77,4 +89,5 @@ const generateToken = (id) => {
 module.exports = {
     registerUser,
     loginUser,
+    getMe,
 };
