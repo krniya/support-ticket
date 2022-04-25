16e5c29a1677580b2dcb7f303fe5156af231f38c
Nitish Kumar <krnitish@live.in>
2022-04-25 23:42:23 +0530

    Designed Home, Register & Login UI
    - Included ReactToast to display error message

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -10,14 +10,12 @@ function Header() {
             <ul>
                 <li>
                     <Link to="/login">
-                        {" "}
                         <FaSignInAlt /> Login
                     </Link>
                 </li>
                 <li>
                     <Link to="/register">
-                        {" "}
-                        <FaUser /> Login
+                        <FaUser /> Register
                     </Link>
                 </li>
             </ul>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,20 @@
+import { FaQuestionCircle, FaTicketAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <>
+            <section className="heading">
+                <h1>What do you need help with?</h1>
+                <p>Please choose from an option below</p>
+            </section>
+            <Link to="/new-ticket" className="btn btn-reverse btn-block">
+                <FaQuestionCircle /> Create New Ticket
+            </Link>
+            <Link to="/ticket" className="btn btn-block">
+                <FaTicketAlt /> View My Tickets
+            </Link>
+        </>
     );
 }
 
