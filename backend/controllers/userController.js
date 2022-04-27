6d87841bda68f63c09dde9176b27726dbd56e8f0
Nitish Kumar <krnitish@live.in>
2022-04-27 23:46:46 +0530

    Backend bug fixed
    - Misplace tokengenerator fixed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -31,7 +31,6 @@ const registerUser = asyncHandler(async (req, res) => {
         name,
         email,
         password: hashedPassword,
-        token: generateToken(user._id),
     });
 
     if (user) {
@@ -39,6 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
             _id: user._id,
             name: user.name,
             email: user.email,
+            token: generateToken(user._id),
         });
     } else {
         res.status(400);
