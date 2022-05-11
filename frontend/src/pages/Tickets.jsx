cc05917be8e39295cd1dc0f3a61855c8f36b1f0d
Nitish Kumar <krnitish@live.in>
2022-05-11 23:29:41 +0530

    Listing of Tickets in UI
    - Store and NewTicket typo fixed

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
+import TicketItem from "../components/TicketItem";
 import { getTickets, reset } from "../features/tickets/ticketSlice";
 
 function Tickets() {
@@ -24,7 +25,23 @@ function Tickets() {
         return <Spinner />;
     }
 
-    return <div>T</div>;
+    return (
+        <>
+            <BackButton url="/" />
+            <h1>Tickets</h1>
+            <div className="tickets">
+                <div className="ticket-headings">
+                    <div>Date</div>
+                    <div>Product</div>
+                    <div>Status</div>
+                    <div></div>
+                </div>
+                {tickets.map((ticket) => (
+                    <TicketItem key={ticket._id} ticket={ticket} />
+                ))}
+            </div>
+        </>
+    );
 }
 
 export default Tickets;
