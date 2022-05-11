62ae0b97fee1cb6d6242c9ac4e9a2bca65489bd8
Nitish Kumar <krnitish@live.in>
2022-05-11 23:08:19 +0530

    Fetch ticket from backend
    - Ticket fetching functionality added to redux

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -14,8 +14,21 @@ const createTicket = async (ticketData, token) => {
     return response.data;
 };
 
+// Get Users Ticket
+const getTickets = async (token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    };
+
+    const response = await axios.get(API_URL, config);
+    return response.data;
+};
+
 const ticketService = {
     createTicket,
+    getTickets,
 };
 
 export default ticketService;
---
+++ frontend/src/pages/Tickets.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import { getTickets, reset } from "../features/tickets/ticketSlice";
+
+function Tickets() {
+    const { tickets, isLoading, isSuccess } = useSelector((state) => state.tickets);
+
+    const dispatch = useDispatch();
+    useEffect(() => {
+        return () => {
+            if (isSuccess) {
+                dispatch(reset());
+            }
+        };
+    }, [dispatch, isSuccess]);
+
+    useEffect(() => {
+        dispatch(getTickets());
+    }, [dispatch]);
+
+    if (isLoading) {
+        return <Spinner />;
+    }
+
+    return <div>T</div>;
+}
+
+export default Tickets;
