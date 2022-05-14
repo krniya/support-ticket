1a240b0acb6824c198130eff50398aa626114856
Nitish Kumar <krnitish@live.in>
2022-05-14 23:46:36 +0530

    Notes backend configured
    - notes controller, models amd routes added

---
+++ backend/controllers/noteController.js
@@ -0,0 +1,61 @@
+const { getTicket } = require("./ticketController");
+
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const Ticket = require("../models/ticketModel");
+const Note = require("../models/noteModel");
+const { red } = require("colors");
+
+// * @desc        Get notes for a ticket
+// * @route       GET /api/tickets/:ticketId/notes
+// * @access      Private
+const getNotes = asyncHandler(async (req, res) => {
+    // Get user using the id in the JWT
+    const user = await User.findById(req.user.id);
+    if (!user) {
+        res.status(401);
+        throw new Error("User not found");
+    }
+    const tickets = await Ticket.findById(req.params.ticketId);
+
+    if (tickets.user.toString() !== req.user.id) {
+        res.status(401);
+        throw new Error("User not authorized");
+    }
+
+    const notes = await Note.find({ ticket: req.params.ticketId });
+
+    res.status(200).json(notes);
+});
+
+// * @desc        Create notes for a ticket
+// * @route       POST /api/tickets/:ticketId/notes
+// * @access      Private
+const addNote = asyncHandler(async (req, res) => {
+    // Get user using the id in the JWT
+    const user = await User.findById(req.user.id);
+    if (!user) {
+        res.status(401);
+        throw new Error("User not found");
+    }
+    const tickets = await Ticket.findById(req.params.ticketId);
+
+    if (tickets.user.toString() !== req.user.id) {
+        res.status(401);
+        throw new Error("User not authorized");
+    }
+
+    const note = await Note.create({
+        text: req.body.text,
+        isStaff: false,
+        ticket: req.params.ticketId,
+        user: req.user.id,
+    });
+
+    res.status(200).json(note);
+});
+
+module.exports = {
+    getNotes,
+    addNote,
+};
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { route } = require("express/lib/application");
 const {
     getTickets,
     getTicket,
@@ -10,6 +9,10 @@ const {
 const router = express.Router();
 
 const { protect } = require("../middleware/authMiddleware");
+
+// Re-route into noterouter
+const noteRouter = require("./noteRoute");
+router.use("/:ticketId/notes", noteRouter);
 
 router.route("/").get(protect, getTickets).post(protect, createTickets);
 router
